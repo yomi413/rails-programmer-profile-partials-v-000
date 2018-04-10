fec1516b2f1a24b96062a0f17284ee887bb3b3ed
Yomaira Escano <yjescano@gmail.com>
2018-04-10 20:39:09 +0000

    automatically backed up by learn

--- spec/views/partials_spec.rb
+++ spec/views/partials_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe "the navbar partial" do
   it "renders the navbar correctly" do
     render "layouts/navbar"
-   
+
     expect(response).to include('Programmers We L<span class="glyphicon glyphicon-heart"></span>ve')
   end
 end
@@ -44,11 +44,11 @@ describe "the attribute partial" do
 
   it "renders any attribute of the programmer" do
       view.lookup_context.prefixes = %w[programmers]
-      assign(:programmer, programmer)  
+      assign(:programmer, programmer)
       render :template => "programmers/show.html.erb"
       expect(response).not_to include("programmer.send(attribute)")
       expect(response).not_to include("programmer.home_country")
-    
+
   end
 
 end
